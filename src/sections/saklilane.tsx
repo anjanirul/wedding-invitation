09086bd4112251b66258f06ad15035574b879d62
Jojo <aditya.abdul@weekendinc.com>
2023-04-21 21:15:45 +0700

    adding icon saklilane

--- src/sections/saklilane.tsx
+++ src/sections/saklilane.tsx
@@ -1,10 +1,14 @@
+import SaklilaneIcon from "@/icons/saklilane";
 import style from "@/styles/saklilane.module.scss";
 
 export default function Saklilane() {
   return (
     <div className={style.saklilaneSection}>
       <div className={style.inner}>
-        <div className="jemina-title text-center mb-2.5">{`sak’lila-ne`}</div>
+        <div className="jemina-title text-center mb-2.5 flex items-center justify-center">
+          {/* {`sak’lila-ne`} */}
+          <SaklilaneIcon />
+        </div>
         <div className="text-center text-sm font-halant">
           Saiki lan liya-liyane // Sekarang dan selama-lamanya.
           <br />
